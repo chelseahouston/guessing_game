3ec12f0e1b76b8ba3041b85c6da68ffb96ab70fb
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-24 09:31:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+myName = input("Hello! What is your name? ")
+number = random.randint(1, 10)
+print("Well, " + myName + " I am thinking of a number between 1 and 10.")
+def main():
+  guess = int(input("Take a guess: ") )
+  if guess == number:
+    print("Good job, " + myName + "! You guessed my number")
+  else:
+    print("Sorry, " + myName + " that's not the number I'm thinking of. Better luck next time!")
+    main()
+main()
