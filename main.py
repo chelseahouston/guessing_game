6898927ba046d72b44a8f763a3e2e5564ce215c2
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-29 18:35:23 +0000

    Added number reveal (and an emoji!)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import emoji
 myName = input("Hello! What is your name? ")
 number = random.randint(1, 10)
 print("Well, " + myName + " I am thinking of a number between 1 and 10. You have 6 attempts to guess it!")
@@ -17,4 +18,7 @@ def main():
     else:
       print(f"Sorry, {myName}, that's not the number I'm thinking of. You have {count} attempt(s) left.")
       count -=1
+  print(f"The correct number was {number}.")
+  print("Congratulations if you got it correct. If not, better luck next time!")
+  print(emoji.emojize(":winking_face_with_tongue:"))
 main()
