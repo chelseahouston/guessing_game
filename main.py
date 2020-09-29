6fcf54e9fa8028211d614b239db801a5209bc7d8
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-29 18:27:12 +0000

    Added attempt countdown, letting user know how many attempts they have left and when they run out of attempts. 6 attempts in total.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 myName = input("Hello! What is your name? ")
 number = random.randint(1, 10)
-print("Well, " + myName + " I am thinking of a number between 1 and 10.")
+print("Well, " + myName + " I am thinking of a number between 1 and 10. You have 6 attempts to guess it!")
 def main():
-  count=0
-  while count < 6:
+  count=5
+  while count > -1:
     guess = input("Take a guess: ")
     try:
       guess = int(guess)
@@ -15,6 +15,6 @@ def main():
       print("Good job, " + myName + "! You guessed my number")
       break
     else:
-      print("Sorry, " + myName + " that's not the number I'm thinking of.")
-      count +=1
+      print(f"Sorry, {myName}, that's not the number I'm thinking of. You have {count} attempt(s) left.")
+      count -=1
 main()
