a524355ddd42196708f4fc22d7edf6ea0f3b212d
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-29 19:10:10 +0000

    All in main def for restarting

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import random
-import emoji
-myName = input("Hello! What is your name? ")
-number = random.randint(1, 10)
-print("Well, " + myName + " I am thinking of a number between 1 and 10. You have 6 attempts to guess it!")
 def main():
+  import random
+  import emoji
+  number = random.randint(1, 10)
+  myName = input("Hello! What is your name? ")
+  print("I am thinking of a number between 1 and 10. You have 6 attempts to guess it!")
   count=5
   while count > -1:
     guess = input("Take a guess: ")
     try:
       guess = int(guess)
     except ValueError:
-      print("Error: Incorrect Value. Please enter a number.")
+      print("Error: Incorrect Value. Please enter a number. Restarting game...")
       main()
     if guess == number:
       print("Good job, " + myName + "! You guessed my number")
@@ -24,4 +24,5 @@ def main():
       print(f"You have {count} attempt(s) left.")
       count -=1
   print(f"The correct number was {number}. " + emoji.emojize(":winking_face_with_tongue:"))
+
 main()
