b53c852948772a0d61cba6c6bf43e2119d6f4cb1
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-29 18:18:03 +0000

    only 6 attempts, added while loop with break

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ myName = input("Hello! What is your name? ")
 number = random.randint(1, 10)
 print("Well, " + myName + " I am thinking of a number between 1 and 10.")
 def main():
-  guess = input("Take a guess: ")
-  try:
-    guess = int(guess)
-  except ValueError:
-    print("Error: Incorrect Value. Please enter a number.")
-    main()
-  if guess == number:
-    print("Good job, " + myName + "! You guessed my number")
-  else:
-    print("Sorry, " + myName + " that's not the number I'm thinking of. Try again!")
-    main()
+  count=0
+  while count < 6:
+    guess = input("Take a guess: ")
+    try:
+      guess = int(guess)
+    except ValueError:
+      print("Error: Incorrect Value. Please enter a number.")
+      main()
+    if guess == number:
+      print("Good job, " + myName + "! You guessed my number")
+      break
+    else:
+      print("Sorry, " + myName + " that's not the number I'm thinking of.")
+      count +=1
 main()
