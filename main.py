5f4003e2113bcb55bde78ab6f36d50deb66274a1
Chelsea Houston <chelseahouston29@gmail.com>
2020-10-01 13:55:43 +0000

    edit comments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
   count=5
   while count > -1:
     guess = input("Take a guess: ")
-    # CONVERT INPUT TO INTEGER - INVALID IF NOT NUMBER 
+    # CONVERT INPUT TO INTEGER - IF NOT NUMBER RESTART  
     try:
       guess = int(guess)
     except ValueError:
@@ -17,18 +17,18 @@ def main():
       main()
     # CORRECT ANSWER
     if guess == number:
-      print("Good job, " + myName + "! You guessed my number")
+      print("Good job, " + myName + "! You guessed my number!")
       break
     # GUESS TOO HIGH
     elif guess > number:
       print(f"Sorry, {myName}, that's not the number I'm thinking of. Your guess is too high!")
       print(f"You have {count} attempt(s) left.")
-      count -=1
+      count = count -1
     # GUESS TOO LOW
     elif guess < number:
       print(f"Sorry, {myName}, that's not the number I'm thinking of. Your guess is too low!")
       print(f"You have {count} attempt(s) left.")
-      count -=1
+      count = count -1
   # REVEAL CORRECT NUMBER
   print(f"The correct number was {number}. " + emoji.emojize(":winking_face_with_tongue:"))
 main()
