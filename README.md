706f3572b507d750dca6377751e100411eecea9a
Chelsea Houston <chelseahouston29@gmail.com>
2020-10-05 15:44:05 +0000

    informed user to install poetry to run project

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # guessing_game
+
+```
+python3 run main.py
+```
+
 Give the user 6 attempts at guessing a random number between 1-10.
 Tell the user if the number they input is too high or too low.
+
+You will need poetry installed to run this code.
--- main.py
+++ main.py
@@ -1,34 +1,46 @@
+# GUESSING GAME --
 def main():
+  # IMPORT --
   import random
   import emoji
+  # END IMPORT --
+
+  # RANDOM NUMBER BETWEEN 1-10 --
   number = random.randint(1, 10)
-  # USER INPUT NAME
+
+  # USER INPUT NAME --
   myName = input("Hello! What is your name? ")
   print(f"Well, {myName}, I am thinking of a number between 1 and 10. You have 6 attempts to guess it!")
-  # GUESS COUNTDOWN NUMBER
+
+  # GUESS COUNTDOWN NUMBER --
   count=5
   while count > -1:
     guess = input("Take a guess: ")
-    # CONVERT INPUT TO INTEGER - IF NOT NUMBER RESTART  
+
+    # CONVERT INPUT TO INTEGER - IF NOT NUMBER RESTART --
     try:
       guess = int(guess)
     except ValueError:
       print("Error: Incorrect Value. Please enter a number. Restarting game...")
       main()
-    # CORRECT ANSWER
+
+    # CORRECT ANSWER --
     if guess == number:
       print("Good job, " + myName + "! You guessed my number!")
       break
-    # GUESS TOO HIGH
+
+    # GUESS TOO HIGH --
     elif guess > number:
       print(f"Sorry, {myName}, that's not the number I'm thinking of. Your guess is too high!")
       print(f"You have {count} attempt(s) left.")
       count = count -1
-    # GUESS TOO LOW
+
+    # GUESS TOO LOW --
     elif guess < number:
       print(f"Sorry, {myName}, that's not the number I'm thinking of. Your guess is too low!")
       print(f"You have {count} attempt(s) left.")
       count = count -1
-  # REVEAL CORRECT NUMBER
+
+  # REVEAL CORRECT NUMBER --
   print(f"The correct number was {number}. " + emoji.emojize(":winking_face_with_tongue:"))
 main()
